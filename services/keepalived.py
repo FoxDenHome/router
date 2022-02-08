206f1f96a82ae5b0820187a7268a1006e19dbd3e
Mark Dietzer <git@doridian.net>
2022-02-07 19:46:40 -0800

    a

--- services/keepalived.py
+++ services/keepalived.py
@@ -1,7 +1,19 @@
+from network import NETWORK_CONFIG
 from service import ServiceTemplate, SystemdService
 
 class KeepalivedService(SystemdService):
+    MASTER_IFACE_TYPES = set(["vlan", "bridge", "standalone"])
+
     def __init__(self):
         super().__init__("keepalived", [
             ServiceTemplate("keepalived.conf", "/etc/keepalived/keepalived.conf")
         ])
+
+    def get_iface(self, network):
+        ifaces_vlan = NETWORK_CONFIG["network_map"][network]
+        ifaces_map = NETWORK_CONFIG["interfaces"]
+
+        for iface in ifaces_vlan:
+            iface_obj = ifaces_map[iface]
+            if iface_obj["type"] in self.MASTER_IFACE_TYPES:
+                return iface
