72cc68c9876138f8c570e5d1a1b78ccce91c1b9f
Mark Dietzer <git@doridian.net>
2022-02-07 17:21:23 -0800

    tmp

--- services/dhcpd.py
+++ services/dhcpd.py
@@ -3,5 +3,5 @@ from service import SystemdService, ServiceTemplate
 class DhcpdService(SystemdService):
     def __init__(self):
         super().__init__("isc-dhcp-server", [
-            ServiceTemplate("dhcpd.conf", "/etc/dhcpd/dhcpd.conf")
+            ServiceTemplate("dhcpd.conf", "/etc/dhcp/dhcpd.conf")
         ])
