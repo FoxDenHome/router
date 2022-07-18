9e8bba601b6a1a67c464696371b32ac0bb78e040
Doridian <git@doridian.net>
2022-07-17 18:48:22 -0700

    add windows script, remove useless files

---
+++ backup.ps1
@@ -0,0 +1,26 @@
+. .\shared.ps1
+
+$CommitMessage = $args[0]
+$BackupDestination = $args[1]
+
+$DumpFileName = "router.tar.gz"
+
+Start-Process -NoNewWindow -RedirectStandardOutput $DumpFileName -Wait -FilePath ssh -ArgumentList @('root@router.foxden.network', 'sysupgrade -b -')
+Remove-Item -Path ".\data" -Recurse -Force
+New-Item -Path ".\data" -ItemType Directory
+
+Exec { tar -xzf $DumpFileName -C ".\data" }
+
+function Do-Redact-Option($OptionKey, $RelativeFile) {
+    $File = "./data$RelativeFile"
+    ((Get-Content -Path "$File" -Raw) -Replace "$OptionKey.*","$OptionKey 'REMOVED'") | Set-Content -Path "$FILE" -NoNewline
+}
+
+Do-Redact-Option 'option private_key' /etc/config/network
+Do-Redact-Option 'option password' /etc/config/ddns
+
+git add -A
+git commit -a -m "$CommitMessage"
+
+Remove-Item -Path "$BackupFile" -Force
+Copy-Item -Path $DumpFileName -Destination "$BackupFile"
