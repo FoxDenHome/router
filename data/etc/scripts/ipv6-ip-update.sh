d2db452cad53baa6f14fab59b40d9420675d8823
Doridian <git@doridian.net>
2022-08-27 07:50:33 -0700

    switch to own v6 tunnel

---
+++ data/etc/scripts/ipv6-ip-update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+. /lib/functions.sh
+. /lib/functions/network.sh
+
+network_get_ipaddr ipaddr WAN
+
+echo "WAN IP = $ipaddr"
+wget -O- "http://10.99.10.1:9999/update-ip?ip=$ipaddr"
