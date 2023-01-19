325c6148dc7647096b015e18d546405bf49e374a
Doridian <git@doridian.net>
2023-01-18 18:51:14 -0800

    no locals

--- synchronize.sh
+++ synchronize.sh
@@ -19,13 +19,12 @@ transfer_section() {
 
 F="$(mktemp)"
 chmod 600 "$F"
-
 transfer_section '/ip/dns/static'
 transfer_section '/ip/dhcp-server/lease'
 transfer_section '/ip/firewall/filter'
 transfer_section '/ip/firewall/nat'
 transfer_section '/ipv6/firewall/filter'
-transfer_section '/system/script' 'name!="init-onboot"' 's~add .* name=init-onboot ~find ~'
+transfer_section '/system/script' '(!(name~"^local-"))' 's~add .* name=local-~find name=~'
 
 scp "$F" router-backup:/tmpfs-scratch/transfer.rsc
 ssh router-backup "/import file-name=tmpfs-scratch/transfer.rsc"
